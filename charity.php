e2df1cb9d2dfb3188f4f1a66914b9ca1ebb9096c
Krista Charner <kristacharner@outlook.com>
2017-07-06 10:01:15 -0400

    added contact modal form and php code to execute sending an email, updated media queries, added new images, created wholesale orders page with image carousel, modified styling of fonts

---
+++ charity.php
@@ -0,0 +1,43 @@
+<?php include_once("corrieshouse.html");
+
+	if (empty($_POST) === false) {
+
+            // echo("submitted");
+            $errors = array();
+
+            $name = $_POST["name"];
+            $email = $_POST["email"];
+            $emailSubject = $_POST["emailSubject"];
+            $message = $_POST["message"];
+
+            if (empty($name) === true || empty($email) === true || empty($emailSubject) === true || empty($message) === true) {
+
+            	$errors[] = "Please fill out all of the fields";
+            }
+            else {
+
+                  if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+                        $errors[] = "Invalid email address, please review";
+                  }
+
+                  if (ctype_alpha($name) === false ) {
+                        $errors[] = "Name field can only contain letters";
+                  }
+            }
+
+            if (empty($errors) === false) {
+                  echo "<ul>";
+                  foreach ($errors as $error) {
+                       echo "<li>", $error, "</li>";
+                  }
+                  echo "</ul>";
+            }
+
+            if (empty($errors) === true) {
+                  mail("hannah@purejoybody.com", $emailSubject, $message, "From: " . $email);
+                  exit();
+            }
+
+        }
+
+?>
